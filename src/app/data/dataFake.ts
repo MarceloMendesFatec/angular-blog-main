044acd14b134348f645544b9de48a2762f433ea5
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2023-05-14 02:03:27 -0300

    inicio projeto

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVA KAWASAKI NINJA 400",
+    "description": "A Kawasaki Ninja 400cc é uma motocicleta esportiva que combina estilo agressivo com desempenho emocionante. Com seu motor potente e design aerodinâmico, a Ninja 400cc oferece uma experiência de pilotagem esportiva e divertida para entusiastas de motos",
+    "photoCover":"https://content2.kawasaki.com/ContentStorage/KMB/ProductTopFeature/1073/d2a7f7a1-8d8a-47bf-8573-ea78fc8bf3a8.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Nova serie da linha Z+",
+    "description": "Preparar, apontar e fogo! a nova serie da linha Z+ da linha Z.",
+    "photoCover":"https://cdn.motor1.com/images/mgl/o6nq2/s1/4x3/kawasaki-z650-2021.webp"
+  }
+]
